c41a6984fe8e925c4ba978251194024242bf5516
Eduardo San Martin Morote <posva13@gmail.com>
2017-04-27 18:46:09 +0200

    ✨ Add HMR for vuex
    Signed-off-by: Eduardo San Martin Morote <posva13@gmail.com>

--- src/store.js
+++ src/store.js
@@ -12,3 +12,21 @@ const store = new Vuex.Store({
 })
 
 export default store
+
+// Hot module replacement for better DX
+if (module.hot) {
+  // accept actions and mutations as hot modules
+  module.hot.accept([
+    './StackOverflow/module'
+  ], () => {
+    // require the updated modules
+    // have to add .default here due to babel 6 module output
+    const stackoverflow = require('./StackOverflow/module').default
+    // swap in the new module
+    store.hotUpdate({
+      modules: {
+        stackoverflow
+      }
+    })
+  })
+}
